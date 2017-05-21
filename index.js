2668434d276fa104ca175f068135a723e45855e5
Matthew Amacker <mamacker@google.com>
2017-05-21 06:17:01 +0000

    Added the ability to look for a specific IP address on the local network as a notice for state.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var ping = require('net-ping');
 var https = require('https');
 var fs = require('fs');
 var storedData = {};
@@ -30,3 +31,30 @@ setInterval(() => {
     });
   });
 }, 3000);
+
+// USe the phone's reserved IP addresses to discover if Matt or Roxanne are home.
+setInterval(() => {
+  var mattSession = ping.createSession({retries:10});
+  var mattTarget = "192.168.1.164"
+  mattSession.pingHost (mattTarget, function (error, target) {
+    if (error)
+      console.log (target + ": " + error.toString ());
+    else {
+      https.get('https://theamackers.com/weasley/set?which=dad&where=home', (res) => {
+      });
+      console.log (target + ": Alive");
+    }
+  });
+
+  var roxanneSession = ping.createSession({retries:10});
+  var roxanneTarget = "192.168.1.96"
+  roxanneSession.pingHost (roxanneTarget, function (error, target) {
+    if (error)
+      console.log (target + ": " + error.toString ());
+    else {
+      https.get('https://theamackers.com/weasley/set?which=mom&where=home', (res) => {
+      });
+      console.log (target + ": Alive");
+    }
+  });
+}, 30000);
