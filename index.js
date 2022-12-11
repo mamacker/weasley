c60fb11b42a60926146c1b3a10cf67990e7de599
Matthew Amacker <matt@mattandroxanne.com>
2022-12-11 11:00:25 -0800

    latest.

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 var ping = require('ping');
 var https = require('https');
 var fs = require('fs');
-var player = require('play-sound')(opts = {})
+var player = require('play-sound')(opts = { players: ['mpg123'] })
 const FauxMo = require('fauxmojs');
 const os = require('os');
 const dns = require('dns');
 
 let ipAddress = '';
-dns.lookup(os.hostname()+".local", (e,a,f) => { 
-  console.log("IP address: ", a, "Errors: ", e); ipAddress = a; 
+dns.lookup(os.hostname() + ".local", (e, a, f) => {
+  console.log("IP address: ", a, "Errors: ", e); ipAddress = a;
   let fauxMo = new FauxMo({
     ipAddress: ipAddress,
     devices: [{
-        name: 'weasley',
-        port: 11000,
-        handler: (action) => {
-          console.log('Weasley:', action);
-          https.get('https://theamackers.com/weasley/set?which=dad&where=work', (res) => { });
-          https.get('https://theamackers.com/weasley/set?which=mom&where=exercise', (res) => { });
-          https.get('https://theamackers.com/weasley/set?which=cole&where=friends', (res) => { });
-          https.get('https://theamackers.com/weasley/set?which=grant&where=school', (res) => { });
+      name: 'weasley',
+      port: 11000,
+      handler: (action) => {
+        console.log('Weasley:', action);
+        https.get('https://theamackers.com/weasley/set?which=dad&where=work', (res) => { });
+        https.get('https://theamackers.com/weasley/set?which=mom&where=exercise', (res) => { });
+        https.get('https://theamackers.com/weasley/set?which=cole&where=friends', (res) => { });
+        https.get('https://theamackers.com/weasley/set?which=grant&where=school', (res) => { });
 
-          setTimeout(() => {
-            https.get('https://theamackers.com/weasley/set?which=dad&where=home', (res) => { });
-            https.get('https://theamackers.com/weasley/set?which=mom&where=home', (res) => { });
-            https.get('https://theamackers.com/weasley/set?which=cole&where=home', (res) => { });
-            https.get('https://theamackers.com/weasley/set?which=grant&where=home', (res) => { });
-          }, 10000);
-        }
+        setTimeout(() => {
+          https.get('https://theamackers.com/weasley/set?which=dad&where=home', (res) => { });
+          https.get('https://theamackers.com/weasley/set?which=mom&where=home', (res) => { });
+          https.get('https://theamackers.com/weasley/set?which=cole&where=home', (res) => { });
+          https.get('https://theamackers.com/weasley/set?which=grant&where=home', (res) => { });
+        }, 10000);
       }
+    }
     ]
   });
 });
@@ -44,7 +44,7 @@ setInterval(() => {
     res.on('end', () => {
       try {
         const parsedData = JSON.parse(rawData);
-        console.log("Data from weasley service: ", parsedData);
+        //console.log("Data from weasley service: ", parsedData);
         var writeCt = 0;
         for (var value in parsedData) {
           if (parsedData[value] != storedData[value]) {
@@ -52,7 +52,7 @@ setInterval(() => {
             setTimeout(((value, parsedData) => {
               return () => {
                 var update = value + " " + parsedData[value];
-                var wstream = fs.createWriteStream('/home/pi/weasley/servofifo.pipe', {flags: 'w', autoClose: true});
+                var wstream = fs.createWriteStream('/home/pi/weasley/servofifo.pipe', { flags: 'w', autoClose: true });
                 wstream.write(update + '\n');
                 wstream.close();
               };
@@ -71,33 +71,32 @@ setInterval(() => {
     console.log(err, err.stack);
   });
 
-  handle.on('error', function(err) {
+  handle.on('error', function (err) {
     console.log("Error trying to connect: ", err);
   });
 }, 3000);
 
 function handlePhone(target) {
   try {
-    switch(target) {
+    switch (target) {
       case targets[0]:
         https.get('https://theamackers.com/weasley/set?which=dad&where=home', (res) => { });
-        console.log (target + ": Alive");
+        console.log(target + ": Alive");
         break;
       case targets[1]:
         https.get('https://theamackers.com/weasley/set?which=mom&where=home', (res) => { });
-        console.log (target + ": Alive");
+        console.log(target + ": Alive");
         break;
     }
-  } catch(ex) { console.log(ex); }
+  } catch (ex) { console.log(ex); }
 }
 
 async function pingPhone(target) {
   let res = await ping.promise.probe(target, {
-	  timeout: 10
+    timeout: 10
   });
-  console.log("Ping res:", res);
   if (res.alive) {
-  	handlePhone(target);
+    handlePhone(target);
   }
 }
 
@@ -115,18 +114,38 @@ checkForPhones();
 var gDuration = 4000;
 function playGongOnce(timeout) {
   if (!timeout) { timeout = gDuration; }
-  player.play("/home/pi/weasley/gong.mp3", {timeout:timeout});
+  player.play("/home/pi/weasley/gong.mp3", { timeout: timeout });
 }
 
-function playFile(filename) {
-  player.play(filename);
+let curPlaying = false;
+async function playFile(filename) {
+  return new Promise((res, rej) => {
+    if (curPlaying) {
+      res();
+      return;
+    }
+
+    curPlaying = true;
+
+    player.play(filename, (err) => {
+      if (err) { rej(err); curPlaying = false; }
+      else {
+        console.log("waiting for audio to be done...");
+        setTimeout(() => {
+          curPlaying = false;
+          res();
+        }, 20000);
+      }
+    });
+  });
 }
 
-function playGongForHour(hour) {
-  playFile("/home/pi/weasley/gongs"+hour + ".mp3");
+async function playGongForHour(hour) {
+  console.log("Playing file: ", hour);
+  return playFile("/home/pi/weasley/gongs" + hour + ".mp3");
 }
 
-setInterval(() => {
+setInterval(async () => {
   var now = new Date();
   var hours = now.getHours();
 
@@ -139,26 +158,26 @@ setInterval(() => {
     hours += 24;
   }
 
-  console.log("Hours: ", hours);
+  console.log("Hours: ", hours, now.getMinutes(), now.getSeconds());
 
   if (hours > 7 && hours < 22) {
     if (now.getMinutes() == 0 && now.getSeconds() <= 10) {
       if (hours > 12) {
         hours = hours - 12;
       }
-      playGongForHour(hours);
+      await playGongForHour(hours);
     }
     if (now.getMinutes() == 30 && now.getSeconds() <= 10) {
-      playFile("/home/pi/weasley/halfgong.mp3");
+      await playFile("/home/pi/weasley/halfgong.mp3");
     }
   }
 }, 10000);
 
 process.on('uncaughtException', function (err) {
-    console.log('----------------------------------------------------------------------');
-    console.log('----------------------------------------------------------------------');
-    console.log('Caught exception: ' + err);
-    console.log(err.stack);
-    console.log('----------------------------------------------------------------------');
-    console.log('----------------------------------------------------------------------');
+  console.log('----------------------------------------------------------------------');
+  console.log('----------------------------------------------------------------------');
+  console.log('Caught exception: ' + err);
+  console.log(err.stack);
+  console.log('----------------------------------------------------------------------');
+  console.log('----------------------------------------------------------------------');
 });
